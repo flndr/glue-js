2f0b5aef4f39cd4b6b8690445dabc5d687936453
TFL <thomas.flander@isotravel.com>
2018-05-08 20:20:11 +0200

    added some thoughts

--- src/Glue/GlueModuleInterface.ts
+++ src/Glue/GlueModuleInterface.ts
@@ -37,6 +37,49 @@ interface GlueModuleInterface {
 
     */
 
+    /*
+    
+        workflow-step
+        
+        input data on start
+            - booking-state LOAD ( ajax-response, contains current booking and current step )
+            - app-state ( from url and/or session )
+        
+        ... user is selecting stuff,
+          entering data to generate/update ...
+        
+        output data before stop
+            - booking-state SAVE ( ajax-response, contains current booking and current step selection )
+            - app-state ( from url and/or session )
+            
+    */
+
+    /*
+        https://reactjs.org/docs/higher-order-components.html
+        build a HOC component that can render
+            hotels, flights, destinations,
+            with options like
+            pick-one (exactly one)
+            pick-some (more than one)
+            pick-one-per-traveler
+            pick-some-per-traveler
+    
+    
+        
+        TOP LAYER --> PRESENTATION : renders the application state with react (virtual dom)
+            - pool of atomic and re-usable stateless components for corporate design ui elements ( buttons, dialogs, layouts )
+            - controller-like components for workflow-steps ()
+        
+        MID LAYER --> STATE & COMUNICATION : mobx, ibe-workflow-ts
+            - state management (ts singletons with mobx power)
+                gets
+            - step and component registry (ibe-workflow-ts)
+            - load and save state from backend (ibe-workflow-ts)
+        
+        BOTTOM LAYER -->  module-glue-ts
+    
+    */
+    
 }
 
 export default GlueModuleInterface;
