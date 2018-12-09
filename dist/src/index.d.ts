2512a9dacf7126fbf282707124013de47ee4d4a3
Thomas Flander <thomas@flander.de>
2018-12-09 06:44:20 +0100

    build

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -3,5 +3,6 @@ import GlueConfig from './Glue/GlueConfig';
 import GlueModule from './Glue/GlueModule';
 import GlueStepper from './Step/GlueStepper';
 import GlueModuleInterface from './Glue/GlueModuleInterface';
-import { load, unload } from './Util/File';
-export { Glue, GlueConfig, GlueModule, GlueModuleInterface, GlueStepper, load, unload };
+import { load } from './Util/File';
+import { createUniqueId } from './Util/Dom';
+export { Glue, GlueConfig, GlueModule, GlueModuleInterface, GlueStepper, load, createUniqueId };
